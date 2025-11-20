640bc0691058f2967d0b008fd69e149d6bd09099
Lalu Fatan <lalufatan25@gmail.com>
2025-11-20 15:39:31 +0700

    menambahkan kode di siswa kt

---
+++ app/src/main/java/com/example/pertemuan8/model/Siswa.kt
@@ -0,0 +1,7 @@
+package com.example.pertemuan8.model // Sesuaikan dengan nama package kamu
+
+data class Siswa(
+    val nama: String = "",
+    val gender: String = "",
+    val alamat: String = ""
+)
