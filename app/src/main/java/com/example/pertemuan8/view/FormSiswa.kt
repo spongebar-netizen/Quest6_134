3c0eeb0100ae344d399f3364f6739a4a7dd94380
Lalu Fatan <lalufatan25@gmail.com>
2025-11-20 16:33:09 +0700

    Menambahkan FormSiswa dengan state rememberSaveable untuk input nama

--- app/src/main/java/com/example/pertemuan8/view/FormSiswa.kt
+++ app/src/main/java/com/example/pertemuan8/view/FormSiswa.kt
@@ -31,3 +31,15 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.pertemuan8.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier:Modifier = Modifier
+){
+    var txtNama by rememberSaveable {mutableStateOf("")}
+    var txtAlamat by remember {mutableStateOf("")}
+    var txtGender by remember {mutableStateOf("")}
+    val listData: MutableList<String> = mutableListOf(txtNama,txtGender,txtAlamat)
