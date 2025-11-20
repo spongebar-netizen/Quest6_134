bc6eb09f4c9cbbb7e946d6af99d8e02436131201
Lalu Fatan <lalufatan25@gmail.com>
2025-11-20 16:36:22 +0700

    menambahkan divider dan RadioButton jenis kelamin

--- app/src/main/java/com/example/pertemuan8/view/FormSiswa.kt
+++ app/src/main/java/com/example/pertemuan8/view/FormSiswa.kt
@@ -72,3 +72,21 @@ fun FormSiswa(
                 onValueChange = {
                     txtNama = it
                 },
+            )
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness, color = Color.Red
+            )
+            Row{
+                pilihanJK.forEach {
+                        item->
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+
+                }
+            }
