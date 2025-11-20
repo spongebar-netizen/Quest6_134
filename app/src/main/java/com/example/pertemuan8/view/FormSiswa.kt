072856c612f88014a9b26da9619a5fb3652bfe34
Lalu Fatan <lalufatan25@gmail.com>
2025-11-20 16:35:11 +0700

    menambahkan layout dan input Nama Lengkap

--- app/src/main/java/com/example/pertemuan8/view/FormSiswa.kt
+++ app/src/main/java/com/example/pertemuan8/view/FormSiswa.kt
@@ -57,3 +57,18 @@ fun FormSiswa(
                     (containerColor = colorResource(id = R.color.teal_700))
             )
         }
+    ){ isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            OutlinedTextField(
+                value = txtNama,
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {
+                    txtNama = it
+                },
